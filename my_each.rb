ec89ffafa72b58365e7d09444d6ebd292e6afaf7
lesliesage <leslie.sage@gmail.com>
2019-04-06 10:43:27 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    counter = 0
+    while counter < collection.length
+     yield collection[counter]
+     counter += 1
+    end
+   collection
+  else
+    nil
+  end
 end
+
+# my test (remove the - from p-uts...)
+# my_each(["a","b","c"]) {|i| p-uts i + "!"}
